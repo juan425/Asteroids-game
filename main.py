03e299618fa1eb28a182fd630819442ab62925fb
juan425 <juanalvarez-1997@hotmail.com>
2025-03-26 00:03:56 -0500

    more changes

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
         pygame.display.flip()
 
+        #limits the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
-    
